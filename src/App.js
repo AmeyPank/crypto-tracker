e761283d15af596d108815f2ed64738eeddebf8b
AmeyPank <pankantiameya@gmail.com>
2023-07-19 02:18:26 +0530

    CryptoTracker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './Components/Common/Footer';
-import Header from './Components/Common/Header';
+import DashboardPage from './Pages/Dashboard';
+import HomePage from './Pages/home';
+import CoinPage from './Pages/Coin';
+import ComparePage from './Pages/Compare';
+import WatchlistPage from './Pages/watchlist';
+
+
+
 
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <Footer/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
